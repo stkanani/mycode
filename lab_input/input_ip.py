572466f6a192dec2307d42fccb1da7b4db874f21
stkanani <sachkanani@gmail.com>
2018-11-05 15:37:04 -0500

    edit 1

---
+++ lab_input/input_ip.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Written By: Sach Kanani
+# Collect IP address information from a user, and display it back to them.
+user_input = input('Please enter an IPv4 IP address:')
+print("You told me the IPv4 address is:" + user_input)
